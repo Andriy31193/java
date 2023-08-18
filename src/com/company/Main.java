f2514c91af392857c4bdc10b7a51651914c4ad2e
Andriy31193 <andriycreator@gmail.com>
2023-08-18 22:01:04 +0000

    task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a month number (1-12): ");
+        int monthNumber = scanner.nextInt();
+
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            String season;
+            if (monthNumber >= 3 && monthNumber <= 5) {
+                season = "Spring";
+            } else if (monthNumber >= 6 && monthNumber <= 8) {
+                season = "Summer";
+            } else if (monthNumber >= 9 && monthNumber <= 11) {
+                season = "Autumn";
+            } else {
+                season = "Winter";
+            }
+
+            System.out.println("The season for month " + monthNumber + " is " + season);
+        } else {
+            System.out.println("Invalid month number. Please enter a value between 1 and 12.");
+        }
     }
 }
+
+
+
